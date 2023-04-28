d0e8acd7fce0a3c102fe45c38593188ed40b1f6e
Surkova Maria <garbeag@yandex>
2023-04-28 12:43:59 +0300

    Домашнее задание задача 1,2,3

--- Program.cs
+++ Program.cs
@@ -17,31 +17,45 @@
 // Задача 13: Напишите программу, которая выводит третью цифру 
 //заданного числа или сообщает, что третьей цифры нет.
 
-int Prog(string msg)
-{
+// int Prog(string msg)
+// {
 
-System.Console.WriteLine($"{msg}");
-return Convert.ToInt32(Console.ReadLine());
-}
-int num = Prog("Введите число");
+// System.Console.WriteLine($"{msg}");
+// return Convert.ToInt32(Console.ReadLine());
+// }
+// int num = Prog("Введите число");
 
-int fnum(int num)
-{
-while (num > 999)
-{
-num /= 10;
-}
-return num % 10;
-}
+// int fnum(int num)
+// {
+// while (num > 999)
+// {
+// num /= 10;
+// }
+// return num % 10;
+// }
 
-bool check(int number)
-{
-if (num < 100)
-return false;
-else return true;
-}
+// bool check(int number)
+// {
+// if (num < 100)
+// return false;
+// else return true;
+// }
 
-if (check(num) != true)
-System.Console.WriteLine("Третьей цифры нет");
-else
-System.Console.WriteLine($"Третья цифра числа {num} -> {fnum(num)}");
+// if (check(num) != true)
+// System.Console.WriteLine("Третьей цифры нет");
+// else
+// System.Console.WriteLine($"Третья цифра числа {num} -> {fnum(num)}");
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+string week (int num)
+{
+    if (num < 1) Console.WriteLine ("Введите корректый день недели от 1 до 7");
+    if (num > 7) Console.WriteLine ("Введите корректый день недели от 1 до 7");
+    if (num > 5) Console.WriteLine ("Сегодня выходной");
+    if (num >= 1 & num < 5) Console.WriteLine ("Сегодня рабочий день");
+    return ("0");
+}
+Console.WriteLine ("Введите день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+week(num);
